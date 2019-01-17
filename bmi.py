bc1aac196b160560f4cdcfdc00d3649358093618
Mickho <mick.my.ho@gmail.com>
2019-01-17 11:00:43 +0800

    first commit

--- bmi.py
+++ bmi.py
@@ -6,8 +6,14 @@ bmi = weight/(hight**2)
 bmi = float(bmi)
 print('Your BMI=', bmi)
 if bmi >= 18.5 and bmi <= 24:
-    print('Your bmi is normal')
+    print('Your bmi is:', bmi, '正常範圍')
 elif bmi < 18.5:
-	print('Your bmi is low')
+	print('Your bmi is:', bmi, '體重過輕')
+elif bmi <= 24 and bmi < 27:
+    print('Your bmi is:', bmi, '過重')
+elif bmi <= 27 and bmi < 30:
+    print('Your bmi is:', bmi, '輕度肥胖')
+elif bmi <= 30 and bmi < 35:
+    print('Your bmi is:', bmi, '中度肥胖')
 else :
-	print('Your bmi is high')
+	print('Your bmi is:', bmi, '重度肥胖')
